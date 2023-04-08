d362bd065de34aecaff15e4eb98e21c2d6aecac6
KUEUNJI <yoo128@naver.com>
2023-04-09 01:44:16 +0900

    menu [메인] - 기념일, 작성 중

--- src/components/EzDay.vue
+++ src/components/EzDay.vue
@@ -1,12 +1,104 @@
 <template>
   <div class="ezday-sec sec">
     <h2 class="sec-tit">기념일</h2>
+    <div class="tab-head">
+      <div class="tab-area">
+        <button class="tab on" @click="tabClick('easyday')">이지데이</button>
+        <button class="tab" @click="tabClick('birthday')">생일자</button>
+      </div>
+      <button class="more-btn">더보기</button>
+    </div>
+    <swiper ref="daySwiper" class="daySwiper" :options="swiperOption" role="itemlist">
+      <swiper-slide v-for="item in test" v-bind:key="item" ref="item">
+        <router-link :to="`/user/${item.E_IDX}`">
+          <div class="img-box">
+            <img :src="`https://easymedia.matty.works:8443/File/Page1/Profile/${item.EMAIL}`" alt="">
+          </div>
+          <div class="txt-box">
+            <p class="name">{{ item.NAME }}</p>
+            <template>
+              <p class="day" v-if="tab == 'easyday'">{{ item.easyday }}</p>
+              <p class="birthday" v-else>{{ item.EI_BIRTH_DATE | MMdd }}</p>
+            </template>
+          </div>
+        </router-link>
+      </swiper-slide>
+    </swiper>
   </div>
 </template>
 
 <script>
-export default {
+import { swiper, swiperSlide } from 'vue-awesome-swiper';
+import 'swiper/dist/css/swiper.min.css';
+import { ezDayApi, birthDayApi } from '@/api/index';
 
+export default {
+  data() {
+    return {
+      swiperOption: {
+        slidesPerView: "auto"
+      },
+      ezDayItems: [],
+      ezBirthItems: [],
+      tab: 'easyday',
+    }
+  },
+  computed: {
+    test() {
+      if(this.tab == 'easyday') {
+        return this.ezDayItems
+      } else {
+        return this.ezBirthItems
+      }
+    }
+  },
+  filters: {
+    MMdd: function(value) {
+      var itemDate = new Date(value);
+      var month =  itemDate.getMonth() + 1;
+      var day =  itemDate.getDate();
+
+      if(month < 10) {
+        month = `0${month}`;
+      }
+      if(day < 10) {
+        day = `0${day}`;
+      }
+      
+      return `${month}월 ${day}일`;
+    }
+  },
+  components: {
+    swiper,
+    swiperSlide
+  },
+  methods: {
+    // tab click
+    tabClick(day) {
+      this.tab = day;
+    }
+  },
+  created() {
+    // 이지데이 API
+    ezDayApi()
+      .then(({ data }) => {
+        this.ezDayItems = data;
+      }) 
+      .catch(error => {
+        console.log("이지데이 에러 : " + error);
+        console.log(error);
+      })
+
+    // 생일 API
+    birthDayApi()
+      .then(({ data }) => {
+        this.ezBirthItems = data;
+      })
+      .catch(error => {
+        console.log("생일 에러 : " + error);
+        console.log(error);
+      })
+  }
 }
 </script>
 
