3b2c3838b0cd5890e14709d3c43a80785fa753f2
KUEUNJI <yoo128@naver.com>
2023-04-09 01:42:32 +0900

    menu [연락처] - 연락처 작성 중

--- src/views/InfoPage.vue
+++ src/views/InfoPage.vue
@@ -1,12 +1,38 @@
 <template>
   <div>
-    <h1>연락처 페이지</h1>
+    <div class="info-sec sec">
+      <div class="">
+        <a href="">
+          <div class="img-box">
+            <img src="" alt="">
+          </div>
+          <div class="txt-box">
+            <p class="name"></p>
+            <p class="info">
+              <span></span> /
+              <span></span>
+            </p>
+          </div>
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+// import {  } from '@/api/index';
 
+export default {
+  // created() {
+  //   InfoApi()
+  //     .then(({ data }) => {
+  //       console.log(data);
+  //     })
+  //     .catch(error => {
+  //       console.log("연락처 에러 : " + error);
+  //       console.log(error);
+  //     })
+  // }
 }
 </script>
 
