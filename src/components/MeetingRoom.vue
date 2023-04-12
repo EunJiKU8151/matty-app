945c28845a509a16ca0e927a79159bf8c231ccbe
KUEUNJI <yoo128@easymedia.net>
2023-04-12 17:00:14 +0900

    메인 - 회의실, css 작성

--- src/components/MeetingRoom.vue
+++ src/components/MeetingRoom.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="meeting-sec sec">
     <h2 class="sec-tit">회의실</h2>
-    <ul>
+    <ul class="meeting-total">
       <li>
         <p class="tit">총 예약</p>
         <p class="txt">0</p>
@@ -19,19 +19,19 @@
         <p class="txt">0</p>
       </li>
     </ul>
-    <ul>
+    <ul class="meeting-list">
       <li v-for="item in meetingRoom" :key="item">
         <p class="roomname">{{ item.RoomName }}</p>
         <div class="bar-box">
           <p class="roomtime">
-            <span>{{ item.Startdate }}</span>
-            <span>{{ item.Enddate }}</span>
+            <span>{{ item.Startdate | HM }}</span> ~ 
+            <span>{{ item.Enddate | HM }}</span>
           </p>
           <div class="bar-wrap">
             <span class="bar"></span>
           </div>
         </div>
-        <div>
+        <div class="profile">
           <div class="img-box">
             <img :src="`https://easymedia.matty.works:8443/File/Page1/Profile/${item.UserId}`" alt="">
           </div>
@@ -49,6 +49,16 @@ export default {
   data() {
     return {
       meetingRoom: [],
+    }
+  },
+  filters: {
+      HM: function(value) {
+      let itemDate = new Date(value);
+      let hours = itemDate.getHours();
+      let minutes  = itemDate.getMinutes();
+      
+      // return `${year}-${month}-${day}`;
+      return `${hours}:${minutes}`;
     }
   },
   created() {
