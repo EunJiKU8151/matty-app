d959478e58f66e21dcfc1f863b657b1f09230172
KUEUNJI <yoo128@easymedia.net>
2023-04-12 16:06:19 +0900

    메인 - 미팅일정, data뿌리기 성공

--- src/components/MeetingSchedule.vue
+++ src/components/MeetingSchedule.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="meeting-sec sec">
     <h2 class="sec-tit">미팅일정</h2>
+    <ul>
+      <li v-for="item in meetingSch" :key="item">
+        <p class="tit">{{ item.Task.T_TITLE }}</p>
+        <div class="cont">
+          <ul class="users">
+            <li v-for="user in item.Workers" :key="user">
+              {{user.TW_NAME}}
+            </li>
+          </ul>
+          <p class="time">{{ item.Task.T_TARGETDATE }}</p>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -8,10 +21,16 @@
 import { meetingSchApi } from '@/api/index.js';
 
 export default {
+  data() {
+    return {
+      meetingSch: []
+    }
+  },
   created() {
     meetingSchApi()
       .then(({ data }) => {
         console.log(data);
+        this.meetingSch = data;
       })
       .catch(error => {
         console.log("미팅일정 에러 : " + error);
